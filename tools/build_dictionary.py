31af350887c0cd0322928436bb06cbf491fbf379
John <johnstalbaum@gmail.com>
2023-10-13 19:20:02 +0900

    hanja nouns

--- tools/build_dictionary.py
+++ tools/build_dictionary.py
@@ -1,30 +1,43 @@
 #!/usr/bin/env python3
 
 # Standard library
-from typing import Dict, List, Set
+from typing import Dict, List, Set, Optional
 import argparse
 import os
 import json
+from dataclasses import dataclass
 
 # pip
 import regex
 
-class HanjaWord:
+class HanjaCharacterEntry:
     def __init__(self):
         self.english_meanings = set()
         self.korean_meanings = set()
+        self.glosses = set()
     english_meanings: Set[str]
     korean_meanings: Set[str]
+    glosses: Set[str]
+
+@dataclass
+class KoreanWord:
+    korean_word: str
+    english_meanings: Set[str]
+    glosses: Set[str]
+    hanja: Optional[str]
 
 def word_reader(file_name: str):
     for row in open(file_name, "r"):
         blob = json.loads(row)
         yield blob
 
-def is_hangul(value):
-    if regex.search(r'\p{IsHangul}', value):
+def is_hangul(value: str):
+    if regex.search(r'\p{IsHangul}', value.replace(" ", "")):
         return True
     return False
+
+def english_meanings_from_links(links: List[str]):
+    return [link[0] for link in links if link[0].replace(" ", "").isalpha() and link[1].replace(" ","").isalpha()]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -66,7 +79,7 @@ if __name__ == "__main__":
     # ko-hanja/new
     # ko-hanja
     # ko-hanja/old
-    all_words: Dict[str, Dict[str, HanjaWord]] = {}
+    all_words: Dict[str, Dict[str, HanjaCharacterEntry]] = {}
     print(f"Parsing Hanja pronunciations and meanings out of words with hanja/new head template.")
     for word in word_reader(in_filename):
         if "head_templates" in word:
@@ -79,28 +92,25 @@ if __name__ == "__main__":
                     if "senses" not in word:
                         continue
                     senses: List[Dict] = word["senses"]
-                    head_template_args: Dict = head_template["args"]
                     for sense in senses:
                         # Links appear to be loosely schematized like this:
                         # [['Hanja', 'hanja#English'], ['견', '견#Korean'], ['dog', 'dog']]
                         if "args" not in head_template:
                             continue
-                        head_template_args = head_template["args"]
+                        head_template_args: Dict = head_template["args"]
                         hangul_pronunciations = []
                         english_meanings = []
+                        glosses = []
                         if "links" in sense:
-                            links: List[List[str]] = []
-                            links += sense["links"]
-                            english_meanings += [link[0] for link in links if link[0].replace(" ", "").isalpha() and link[1].replace(" ","").isalpha()]
+                            links: List[str] = sense["links"]
+                            english_meanings += english_meanings_from_links(links)
+                            hangul_pronunciations += [link[0] for link in links if link[1][1:] == '#Korean' and link[0] != hanja_word and is_hangul(link[0])]
                         korean_meanings = [head_template_args[key] for key in head_template_args if len(head_template_args[key]) > 0]
                         hangul_pronunciations += [meaning for meaning in korean_meanings if len(meaning) == 1]
 
                         if "glosses" in sense:
-                            glosses = sense["glosses"]
-                            english_meanings += glosses
+                            glosses += sense["glosses"]
                         
-                        if len(english_meanings) == 0:
-                            continue
                         if len(hangul_pronunciations) == 0:
                             continue
                         if len(korean_meanings) == 0:
@@ -109,10 +119,55 @@ if __name__ == "__main__":
                             all_words[hanja_word] = {}
                         for hangul_pronunciation in set(hangul_pronunciations):
                             if hangul_pronunciation not in all_words[hanja_word]:
-                                all_words[hanja_word][hangul_pronunciation] = HanjaWord()
+                                all_words[hanja_word][hangul_pronunciation] = HanjaCharacterEntry()
 
                             for english_meaning in english_meanings:
                                 all_words[hanja_word][hangul_pronunciation].english_meanings.add(english_meaning)
                             for korean_meaning in korean_meanings:
                                 all_words[hanja_word][hangul_pronunciation].korean_meanings.add(korean_meaning)
+                            for gloss in glosses:
+                                all_words[hanja_word][hangul_pronunciation].korean_meanings.add(gloss)
     print(f"Acquired {len(all_words)} unique hanja words.")
+
+    print(f"Parsing Hanja nouns")
+    korean_words: List[KoreanWord] = []
+    for word in word_reader(in_filename):
+        if "head_templates" in word:
+            head_templates: List[Dict] = word["head_templates"]
+            for head_template in head_templates:
+                head_template_name = head_template["name"]
+                if head_template_name in ("ko-noun"):
+                    focus_word = word["word"]
+                    if is_hangul(focus_word):
+                        pass
+                    elif focus_word.replace(" ","").isalpha() and focus_word.replace(" ","").isascii():
+                        pass
+                    else:
+                        glosses = []
+                        english_meanings = []
+                        hangul_pronunciations = []
+                        hanja_word = focus_word
+                        if "args" in head_template:
+                            head_template_args: Dict = head_template["args"]
+                            if 'hangul' in head_template_args:
+                                hangul_pronunciations.append(head_template_args["hangeul"])
+                        if "forms" in word:
+                            forms: List[Dict] = word["forms"]
+                            for form in forms:
+                                if 'hangeul' in form['tags'] and 'form' in form:
+                                    hangul_pronunciations.append(form['form'])
+                        if 'senses' in word:
+                            senses = word['senses']
+                            for sense in senses:
+                                if 'links' in sense:
+                                    links = sense['links']
+                                    english_meanings += english_meanings_from_links(links)
+                                    hangul_pronunciations += [link[0] for link in links if is_hangul(link[1].replace('#Korean', ""))]
+                                if len(set(hangul_pronunciations)) < 1:
+                                    print("got less than 1 hangul pronunciation")
+                                    print(hangul_pronunciations)
+                                    continue
+                                if 'glosses' in sense:
+                                    glosses += sense['glosses']
+                                korean_words.append(KoreanWord(hangul_pronunciations[0], set(english_meanings), set(glosses), hanja_word))
+    print(f"Acquired {len(korean_words)} unique hanja-based Korean words.")
